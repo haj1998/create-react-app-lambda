22571d5de7d65fb89529f1edf3025d162c222cf7
haj1998 <36560955+haj1998@users.noreply.github.com>
2021-02-10 20:35:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = <Welcome name="FAIZAL" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
